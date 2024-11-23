f58348431264842d46f52f18879088365d8ec2a9
roshanlimbu <topazkayle@gmail.com>
2024-11-23 17:58:48 +0545

    fixed key error

--- client/src/pages/admin-view/dashboard.jsx
+++ client/src/pages/admin-view/dashboard.jsx
@@ -1,20 +1,17 @@
 import ProductImageUpload from "@/components/admin-view/image-upload";
 import { Button } from "@/components/ui/button";
-import {
-  addFeatureImage,
-  getFeatureImages,
-  deleteFeatureImage,
-} from "@/store/common-slice";
+import { addFeatureImage, getFeatureImages } from "@/store/common-slice";
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { FaTrash } from "react-icons/fa";
 
 function AdminDashboard() {
   const [imageFile, setImageFile] = useState(null);
   const [uploadedImageUrl, setUploadedImageUrl] = useState("");
   const [imageLoadingState, setImageLoadingState] = useState(false);
   const dispatch = useDispatch();
-  const { featureImageList = [] } = useSelector((state) => state.commonFeature);
+  const { featureImageList } = useSelector((state) => state.commonFeature);
+
+  // console.log(uploadedImageUrl, "uploadedImageUrl");
 
   function handleUploadFeatureImage() {
     dispatch(addFeatureImage(uploadedImageUrl)).then((data) => {
@@ -26,17 +23,11 @@ function AdminDashboard() {
     });
   }
 
-  function handleDeleteFeatureImage(id) {
-    dispatch(deleteFeatureImage(id)).then((data) => {
-      if (data?.payload?.success) {
-        dispatch(getFeatureImages());
-      }
-    });
-  }
-
   useEffect(() => {
     dispatch(getFeatureImages());
   }, [dispatch]);
+
+  // console.log(featureImageList, "featureImageList");
 
   return (
     <div>
@@ -48,25 +39,19 @@ function AdminDashboard() {
         setImageLoadingState={setImageLoadingState}
         imageLoadingState={imageLoadingState}
         isCustomStyling={true}
+      // isEditMode={currentEditedId !== null}
       />
       <Button onClick={handleUploadFeatureImage} className="mt-5 w-full">
         Upload
       </Button>
       <div className="flex flex-col gap-4 mt-5">
-        {featureImageList.length > 0
+        {featureImageList && featureImageList.length > 0
           ? featureImageList.map((featureImgItem) => (
             <div key={featureImgItem._id} className="relative">
               <img
                 src={featureImgItem.image}
-                alt="Feature"
                 className="w-full h-[300px] object-cover rounded-t-lg"
               />
-              <button
-                onClick={() => handleDeleteFeatureImage(featureImgItem._id)}
-                className="absolute top-2 right-2 bg-red-600 text-white p-2 rounded-full"
-              >
-                <FaTrash />
-              </button>
             </div>
           ))
           : null}
